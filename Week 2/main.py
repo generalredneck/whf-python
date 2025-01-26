1919082d5f698015fccb616d64f32dcc8a2b69ed
Allan Chappell <generalredneck@gmail.com>
2025-01-26 09:19:15 -0600

    initial commit

---
+++ Week 2/main.py
@@ -0,0 +1,13 @@
+print("Welcome to the tip calculator!")
+
+# TODO: We are inputing numbers, but what does input give us?
+
+bill = input("What is the total bill amount?\n$:")
+tip = input("How much tip would you like to give?\nPercent:")
+split = input("How many people to split the bill?\nPeople:")
+
+# TODO: Total is the amount of the tip plus the bill, then split by the number of people.
+
+total = 0
+
+print(f"Each person should pay: ${total}")
