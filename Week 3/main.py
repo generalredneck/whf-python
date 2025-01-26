d3a430a4756d9bbdf8f2f03e9b81e5897c4f1372
Allan Chappell <generalredneck@gmail.com>
2025-01-26 09:49:35 -0600

    adding week3

---
+++ Week 3/main.py
@@ -0,0 +1,6 @@
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+    else:
+        print(f"Congratulations! You guessed the number {number}!")
